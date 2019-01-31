153ebc08cbbcce7661c6689bff094260ca22c17b
David McGrann <dpmcgrann@gmail.com>
2019-01-31 19:14:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function loadIssue() {
     created_at: "2016-03-31 16:23:13 UTC",
     body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
   }
- 
-   document.getElementsByTagName("main")[0].innerHTML += result;
+
+  
 }
