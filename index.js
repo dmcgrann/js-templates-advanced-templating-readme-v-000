a0508b342c00e3110bd7e600b13930e451c0b837
David McGrann <dpmcgrann@gmail.com>
2019-01-31 19:13:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function loadIssue() {
     body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
   }
  
- var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for(var i=0;i<issues.length;i++) {
-    var result = template(issues[i]);
+  var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  var result = template(issue);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
